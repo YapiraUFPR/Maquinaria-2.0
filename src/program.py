c2404d4ce1ed2161a8609c92931236b4176d22d8
gab <gabriel.pontarolo045@gmail.com>
2023-07-15 12:18:12 -0300

    fix map json import and export

--- src/program.py
+++ src/program.py
@@ -36,11 +36,15 @@ args = parser.parse_args()
 ############################# DEFINES #############################
 ###################################################################
 
-# encoder values
+# encoder and mapping values
 STEPS_NUMBER = 7 * 20
 RPM = 800
-RADIUS_WHEEL = 1.65  # centimeters
-# RADIUS_WHEEL = 16.5 # millimeters
+RADIUS_WHEEL = 1.6  # cm
+STATIC_COEFICIENT = 1
+AXIS_DISTANCE = 10  # cm
+A = 1
+B = 1
+MAP_FNAME = "map.json"
 
 # pins setup
 clockwise_pin_1 = 13
@@ -238,14 +242,16 @@ def stop_for_mark_callback():
     print(">>", end="")
 
 
-def map_callback():
+def map_callback(load=False):
     global should_map
     global track_map
     global encoder_ml
     global encoder_mr
 
     should_map = True
-    track_map = TrackMap(encoder_mr, encoder_ml, 1, 1)
+    track_map = TrackMap(encoder_mr, encoder_ml, A, B, LINEAR_SPEED, STATIC_COEFICIENT, AXIS_DISTANCE)
+    if load:
+        track_map.from_file(MAP_FNAME)
 
     print("WILL MAP")
     print(">>", end="")
@@ -453,17 +459,19 @@ def process_frame(image_input, last_res_v):
     global last_error
     global just_seen_line
     global right_mark_buffer_count
+
     global should_move
     global should_stop
     global should_stop_for_distance
+    global should_map
+    global should_use_map
+
     global track_length
     global right_mark_count
     global init_time
     global lost
     global no_movement_count
     global after_loss_count
-    global should_map
-    global should_use_map
     global track_map
     global total_distance
 
@@ -518,6 +526,10 @@ def process_frame(image_input, last_res_v):
         #     after_loss_count = 0
 
         lost = False
+
+        # if should_use_map:
+        #     global track_map
+        #     linear = track_map.track_map[mark_count].pwm_speed
 
         if abs(error) > CURVE_ERROR_THRH:
             linear = LINEAR_SPEED_ON_CURVE
@@ -768,8 +780,8 @@ def main():
     if args.map:
         map_callback()
 
-    # if args.usemap != None:
-    #    load_map(args.usemap)
+    if args.usemap != None:
+        map_callback(load=True)
     ##############################
 
     last_res_v = {"left": 0, "right": 0}
--- src/tests/fisheye.py
+++ src/tests/fisheye.py
@@ -44,7 +44,7 @@ retval, K, D, rvecs, tvecs = cv2.fisheye.calibrate(
     calibration_flags,
     (cv2.TERM_CRITERIA_EPS+cv2.TERM_CRITERIA_MAX_ITER, 30, 1e-6))
 
-img = cv2.imread(img_path)
+img = cv2.imread("img.png")
 img_dim = img.shape[:2][::-1]  
 
 DIM = img_dim 
@@ -52,7 +52,7 @@ DIM = img_dim
 scaled_K = K * img_dim[0] / DIM[0]  
 scaled_K[2][2] = 1.0  
 new_K = cv2.fisheye.estimateNewCameraMatrixForUndistortRectify(scaled_K, D,
-    img_dim, np.eye(3), balance=balance)
+    img_dim, np.eye(3), balance=1)
 map1, map2 = cv2.fisheye.initUndistortRectifyMap(scaled_K, D, np.eye(3),
     new_K, img_dim, cv2.CV_16SC2)
 undist_image = cv2.remap(img, map1, map2, interpolation=cv2.INTER_LINEAR,
